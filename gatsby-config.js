ffc157766e9c11a13cbc175c0e63823b9254206b
Ramesh G <ramesh.guthula.kkd@mgmail.com>
2021-03-04 15:02:52 +0530

    page modification working locally

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,8 +80,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Codeclust.com...`,
+        short_name: `Codeclust.com...`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
